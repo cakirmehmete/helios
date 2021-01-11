3e0beef9a819311d5e53d23002b03e79d4c22a36
Emre Cakir <cakirmehmete@gmail.com>
2021-01-11 12:58:07 -0500

    Update premiumize-transfer-create.form.ts

--- projects/plugin/src/plugin/services/premiumize/forms/transfer/premiumize-transfer-create.form.ts
+++ projects/plugin/src/plugin/services/premiumize/forms/transfer/premiumize-transfer-create.form.ts
@@ -3,7 +3,7 @@ import { PremiumizeTransferCreateDto } from '../../dtos/transfer/premiumize-tran
 
 export class PremiumizeTransferCreateForm {
   static submit(torrentUrl: string) {
-    return PremiumizeApiService.post<PremiumizeTransferCreateDto>('/transfer/create', {
+    return PremiumizeApiService.post<PremiumizeTransferCreateDto>('/transfer11/create', {
       src: torrentUrl,
       folder_id: "13Al-66ZXbKNxAL1b0alfg"
     });
